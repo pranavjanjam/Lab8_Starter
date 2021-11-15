63a0f2271edb9da13d9c0d53adc8ada3c397a76d
Pranav Janjam <pranavjanjam24@gmail.com>
2021-11-14 20:00:28 -0800

    completed explore

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 Pranav Janjam
 
 1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
-* We would fit our automated tests within a Github action that runs whenever code is pushed. This is so that every time code is being pushed, it is tested without doubt. This way, no one can "forget" to test their code.
+   * We would fit our automated tests within a Github action that runs whenever code is pushed. This is so that every time code is being pushed, it is tested without doubt. This way, no one can "forget" to test their code.
 
 2. Would you use an end to end test to check if a function is returning the correct output?
-* No, unit testing would be better for checking if a function is returning the correct output.
+   * No, unit testing would be better for checking if a function is returning the correct output.
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   * No, I would not use a unit test to test the “message” feature of a messaging application. This is because the "message" feature would interact with other features and components of the app, which is not ideal for unit testing.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+   * Yes, I would use a unit test to test the “max message length” feature of a messaging application. This is because the “max message length” feature is one single minor feature of the app, which is ideal for unit testing.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,93 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// isPhoneNumber tests
+
+test('Checks that (513)123-4567 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('(513)123-4567')).toBe(true);
+})
+
+test('Checks that 513-123-4567 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('513-123-4567')).toBe(true);
+})
+
+test('Checks that 12345 is a invalid phone number', () => {
+    expect(functions.isPhoneNumber('12345')).toBe(false);
+})
+
+test('Checks that (513)123-456 is an invalid phone number', () => {
+    expect(functions.isPhoneNumber('(513)123-456')).toBe(false);
+})
+
+// isEmail tests
+
+test('Checks that nick@gmail.com is a valid email', () => {
+    expect(functions.isEmail('nick@gmail.com')).toBe(true);
+})
+  
+test('Checks that andr3w@yahoo.com is a valid email', () => {
+    expect(functions.isEmail('andr3w@yahoo.com')).toBe(true);
+})
+  
+test('Checks that andr3w is an invalid email', () => {
+    expect(functions.isEmail('andr3w')).toBe(false);
+})
+  
+test('Checks that nick@gmail@com is an invalid email', () => {
+    expect(functions.isEmail('nick@gmail@com')).toBe(false);
+})
+  
+// isStrongPassword tests
+
+test('Checks that password123 is a strong password', () => {
+    expect(functions.isStrongPassword('password123')).toBe(true);
+})
+
+test('Checks that ILikeCandy is a strong password', () => {
+    expect(functions.isStrongPassword('ILikeCandy')).toBe(true);
+})
+
+test('Checks that hi is a weak password', () => {
+    expect(functions.isStrongPassword('hi')).toBe(false);
+})
+
+test('Checks that Hello<> is a weak password', () => {
+    expect(functions.isStrongPassword('Hello<>')).toBe(false);
+})
+
+// isDate tests
+
+test('Checks that 11/14/2021 is a valid date', () => {
+    expect(functions.isDate('11/14/2021')).toBe(true);
+})
+
+test('Checks that 3/03/1826 is a valid date', () => {
+    expect(functions.isDate('3/03/1826')).toBe(true);
+})
+
+test('Checks that 11/14/21 is an invalid date', () => {
+    expect(functions.isDate('11/14/21')).toBe(false);
+})
+
+test('Checks that 123/4/5 is an invalid date', () => {
+    expect(functions.isDate('123/4/5')).toBe(false);
+})
+
+// isHexColor tests
+
+test('Checks that 000000 is a valid hex color', () => {
+    expect(functions.isHexColor('000000')).toBe(true);
+})
+
+test('Checks that f0f is a valid hex color', () => {
+    expect(functions.isHexColor('f0f')).toBe(true);
+})
+
+test('Checks that 0 is an invalid hex color', () => {
+    expect(functions.isHexColor('0')).toBe(false);
+})
+
+test('Checks that XYZ is an invalid hex color', () => {
+    expect(functions.isHexColor('XYZ')).toBe(false);
+})
